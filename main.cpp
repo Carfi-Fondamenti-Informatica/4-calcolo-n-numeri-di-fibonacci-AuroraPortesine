59783d3d473a20bc411107c1306d32fa52b5b174
AuroraPortesine <100139517+AuroraPortesine@users.noreply.github.com>
2022-04-02 14:31:06 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,17 @@
 using namespace std;
 
 int main() {
-   return 0;
+    int a=0, b=1, c=0, n=0, i;
+    cin >> n;
+
+    if(n>=2){
+        for(i=0;i<n; i++){
+            c=a+b;
+            b=a;
+            a=c;
+            cout<<c<<endl;
+        }
+    }else{
+        cout << "errore"<< endl;
+    }
 }
